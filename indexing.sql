115ad8220c7cc4f38d5c5e574d6f15fe4f33e866
gchu <gchu647@gmail.com>
2018-07-31 12:40:24 -1000

    doing the indexing

--- indexing.sql
+++ indexing.sql
@@ -1,32 +1,34 @@
 -- CREATE USER indexed_cars_user;
 -- CREATE DATABASE index_cars OWNER indexed_cars_user;
 
-/* 1) 44.3 ms
+-- CREATE INDEX make_code_idx ON car_models(make_code)
+
+/* initial: 44.3 ms
 SELECT DISTINCT make_title
 FROM car_models
 WHERE make_code='LAM';
 */
 
-/* 2) 51.2 ms
+/* initial: 51.2 ms
 SELECT DISTINCT model_title
 FROM car_models
 WHERE make_code='NISSAN' AND model_code='GT-R';
 */
 
-/* 3) 46.9 ms
+/* initial: 46.9 ms
 SELECT make_code, model_code, model_title, year
 FROM car_models
 WHERE make_code='LAM';
 */
 
-/* 4) 50.2 ms
-SELECT COUNT(*)
+/* initial: 151 ms
+SELECT *
 FROM car_models
 WHERE year BETWEEN 2010 AND 2015;
 */
 
-/* 5) 48.9 ms
-SELECT COUNT(*)
+/* initial: 67 ms
+SELECT *
 FROM car_models
 WHERE year=2010;
 */
