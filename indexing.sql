f37d39f32966619bd191418251dcf7bfc50948b9
gchu <gchu647@gmail.com>
2018-07-31 13:59:24 -1000

    added index and measure NEW time

--- indexing.sql
+++ indexing.sql
@@ -1,34 +1,38 @@
 -- CREATE USER indexed_cars_user;
 -- CREATE DATABASE index_cars OWNER indexed_cars_user;
 
--- CREATE INDEX make_code_idx ON car_models(make_code)
 
-/* initial: 44.3 ms
-SELECT DISTINCT make_title
-FROM car_models
-WHERE make_code='LAM';
-*/
+-- CREATE INDEX IF NOT EXISTS make_code_idx 
+-- ON car_models(make_code);
 
-/* initial: 51.2 ms
-SELECT DISTINCT model_title
-FROM car_models
-WHERE make_code='NISSAN' AND model_code='GT-R';
-*/
+-- CREATE INDEX IF NOT EXISTS make_and_model_code_idx 
+-- ON car_models(make_code, model_code);
 
-/* initial: 46.9 ms
-SELECT make_code, model_code, model_title, year
-FROM car_models
-WHERE make_code='LAM';
-*/
+-- CREATE INDEX IF NOT EXISTS year_idx 
+-- ON car_models(year);
 
-/* initial: 151 ms
-SELECT *
-FROM car_models
-WHERE year BETWEEN 2010 AND 2015;
-*/
 
-/* initial: 67 ms
-SELECT *
-FROM car_models
-WHERE year=2010;
-*/
+--initial: 44.3ms, New: 0.9ms
+-- SELECT DISTINCT make_title
+-- FROM car_models
+-- WHERE make_code='LAM';
+
+-- initial: 51.2ms, New: 0.61ms
+-- SELECT DISTINCT model_title
+-- FROM car_models
+-- WHERE make_code='NISSAN' AND model_code='GT-R';
+
+-- initial: 46.9ms, New: 1.69ms
+-- SELECT make_code, model_code, model_title, year
+-- FROM car_models
+-- WHERE make_code='LAM';
+
+-- initial: 151ms, New: 125ms
+-- SELECT *
+-- FROM car_models
+-- WHERE year BETWEEN 2010 AND 2015;
+
+-- initial: 67ms, New: 31.36ms
+-- SELECT *
+-- FROM car_models
+-- WHERE year=2010;
